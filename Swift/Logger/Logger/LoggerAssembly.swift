38eaa1aa1cdf6f8c00b3ab5ffd725f8212c6c699
Ivlev Alexander <ivlev.stef@gmail.com>
2016-09-28 18:41:04 +0700

    Added missing sample logger...

---
+++ Swift/Logger/Logger/LoggerAssembly.swift
@@ -0,0 +1,20 @@
+//
+//  LoggerAssembly.swift
+//  Logger
+//
+//  Created by Alexander Ivlev on 26/09/16.
+//  Copyright © 2016 Alexander Ivlev. All rights reserved.
+//
+
+import DITranquillity
+
+public final class LoggerAssembly: DIAssembly {
+	public var publicModules: [DIModule] { return [ LoggerModule() ] }
+	
+	public var modules: [DIModule] { return [ ] }
+	
+	public var dependencies: [DIAssembly] { return [ ] }
+	
+	public init() {}
+}
+
