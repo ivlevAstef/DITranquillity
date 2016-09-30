2c10d0b224f156ea90470a213ef436731a7cdcd2
Ivlev Alexander <ivlev.stef@gmail.com>
2016-09-30 11:35:29 +0700

    fixed

--- SetupDependencies/Package.swift
+++ SetupDependencies/Package.swift
@@ -1,6 +1,7 @@
 import PackageDescription
 
 let package = Package(
+	name: "DITranquillity_Download",
 	dependencies: [
 		.Package(url: "https://github.com/ivlevAstef/DITranquillity", majorVersion: 0),
   ]
