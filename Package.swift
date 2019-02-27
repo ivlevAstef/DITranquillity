f303c5d59720d0fb581bfa77d3061e3955f8931c
Ивлев Александр Евгеньевич <al.ivlev@cft.ru>
2019-02-27 16:48:51 +0700

    fix swift pm version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     products: [.library(name: "DITranquillity", targets: ["DITranquillity"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ivlevAstef/SwiftLazy.git", from: "1.2.1")
+        .package(url: "https://github.com/ivlevAstef/SwiftLazy.git", from: "1.1.2")
     ],
     targets: [
         .target(name: "DITranquillity", dependencies: ["SwiftLazy"]),
